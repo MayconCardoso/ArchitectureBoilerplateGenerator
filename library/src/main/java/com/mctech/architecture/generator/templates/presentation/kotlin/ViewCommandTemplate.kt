c70fb6cca89abf6967b03bd6db41abfd90dd0753
mayconcardoso <maycon.santos.cardoso@gmail.com>
2019-12-02 23:11:59 -0300

    - Starting presentation layer.

---
+++ library/src/main/java/com/mctech/architecture/generator/templates/presentation/kotlin/ViewCommandTemplate.kt
@@ -0,0 +1,26 @@
+package com.mctech.architecture.generator.templates.presentation.kotlin
+
+import com.mctech.architecture.generator.path.ModuleFilePath
+import com.mctech.architecture.generator.settings.featureEntityName
+import com.mctech.architecture.generator.templates.Template
+import java.io.PrintWriter
+
+/**
+ * @author MAYCON CARDOSO on 2019-11-27.
+ */
+class ViewCommandTemplate(modulePath: ModuleFilePath) : Template(modulePath, false) {
+
+    override val folder: String
+        get() = "entity"
+
+    override val className: String
+        get() = "${featureEntityName()}ViewModel"
+
+    override fun generateImports(output: PrintWriter) = Unit
+
+    override fun generateClassName(output: PrintWriter) {
+        output.println("class $className")
+    }
+
+    override fun generateClassBody(output: PrintWriter) = Unit
+}
