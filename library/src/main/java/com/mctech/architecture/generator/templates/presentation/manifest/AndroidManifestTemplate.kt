0af5a6205c3a4a5ec461309938a0db64d47f3886
mayconcardoso <maycon.santos.cardoso@gmail.com>
2019-12-05 21:42:07 -0300

    - AndroidManifest.xml file generated

--- library/src/main/java/com/mctech/architecture/generator/templates/presentation/manifest/AndroidManifestTemplate.kt
+++ library/src/main/java/com/mctech/architecture/generator/templates/presentation/manifest/AndroidManifestTemplate.kt
@@ -1,8 +1,11 @@
 package com.mctech.architecture.generator.templates.presentation.manifest
 
 import com.mctech.architecture.generator.generator.FileGenerator
+import com.mctech.architecture.generator.generator.printTabulate
+import com.mctech.architecture.generator.generator.writeFile
 import com.mctech.architecture.generator.path.FilePath
 import com.mctech.architecture.generator.path.ModuleFilePath
+import com.mctech.architecture.generator.settings.featurePackage
 import com.mctech.architecture.generator.settings.featureSegment
 
 /**
@@ -10,10 +13,18 @@ import com.mctech.architecture.generator.settings.featureSegment
  */
 class AndroidManifestTemplate(private val moduleFilePath: ModuleFilePath) : FilePath, FileGenerator {
     override fun getPath(): String {
-        return "Generating file: features/feature-${featureSegment()}/${moduleFilePath.type.getMainFolder()}AndroidManifest.xml"
+        return "features/feature-${featureSegment()}/${moduleFilePath.type.getMainFolder()}AndroidManifest.xml"
     }
 
-    override fun generate() {
-        println(getPath())
+    override fun generate() = writeFile(this){ output ->
+        output.println("<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\"")
+        output.printTabulate("package=\"${featurePackage()}\">")
+
+        output.printTabulate("<application>")
+        output.printTabulate(countTabulate = 2, value = "<activity android:name=\".TesteActivity\" />")
+        output.printTabulate("</application>")
+
+        output.println("</manifest>")
+
     }
 }
