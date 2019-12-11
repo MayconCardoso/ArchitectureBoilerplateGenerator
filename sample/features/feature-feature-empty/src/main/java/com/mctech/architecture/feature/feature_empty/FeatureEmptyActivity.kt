d6063c660d6826c5ec5b048f1e5c38d661815c42
Maycon Cardoso <maycon.santos.cardoso@gmail.com>
2019-12-11 12:50:05 -0300

    - Sample example of empty feature

--- sample/features/feature-feature-empty/src/main/java/com/mctech/architecture/feature/feature_empty/FeatureEmptyActivity.kt
+++ sample/features/feature-feature-empty/src/main/java/com/mctech/architecture/feature/feature_empty/FeatureEmptyActivity.kt
@@ -1,6 +1,5 @@
 package com.mctech.architecture.feature.feature_empty
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class FeatureEmptyActivity : AppCompatActivity() {
